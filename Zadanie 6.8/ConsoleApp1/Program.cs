5612d88026b364fd50f22afb4b03629ffb770e44
Maciowski <132011594+Maciowski@users.noreply.github.com>
2023-05-16 23:49:48 +0200

    Update Program.cs

--- Zadanie 6.8/ConsoleApp1/Program.cs
+++ Zadanie 6.8/ConsoleApp1/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 
 public class Prostopadloscian
 {
     private double dlugosc;
     private double szerokosc;
     private double wysokosc;
+
 
     public Prostopadloscian(double dlugosc, double szerokosc, double wysokosc)
     {
@@ -41,27 +42,31 @@ public class Program
 {
     public static void Main()
     {
+
         Prostopadloscian p1 = new Prostopadloscian(2, 3, 4);
         Prostopadloscian p2 = new Prostopadloscian(3, 4, 5);
 
+
         Console.WriteLine("Objetosc p1: " + p1.ObliczObjetosc());
         Console.ReadKey();
+
         Console.WriteLine("Objetosc p2: " + p2.ObliczObjetosc());
         Console.ReadKey();
-        int wynikPorownania = p1.PorownajObjetosci(p2);
+
+        int wynikPorownania =  p1.PorownajObjetosci(p2);
         if (wynikPorownania == 1)
         {
-            Console.WriteLine("Prostopadłościan p1 ma większą objętość niż p2.");
+            Console.WriteLine("Prostopadloscian p1 ma wieksza objetosc niz p2.");
             Console.ReadKey();
         }
         else if (wynikPorownania == -1)
         {
-            Console.WriteLine("Prostopadłościan p2 ma większą objętość niż p1.");
+            Console.WriteLine("Prostopadloscian p2 ma wieksza objetosc niz p1");
             Console.ReadKey();
         }
         else
         {
-            Console.WriteLine("Oba prostopadłościany mają taką samą objętość.");
+            Console.WriteLine("Oba prostopadlosciany maja taka sama objetosc");
             Console.ReadKey();
         }
     }
